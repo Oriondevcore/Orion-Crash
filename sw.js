5b0a32a4022702797353a4cd33a70c9eff740157
Graham Schubach <graham@oriondevcore.com>
2026-01-11 06:57:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-const CACHE_NAME = 'orion-crash-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'crash.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=index.html">
+  </head>
+</html>
